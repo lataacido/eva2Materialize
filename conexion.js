e1cb3fd8f8512c2ff77647d35cc74d0935bc17a8
Beginning0 <39624026+Beginning0@users.noreply.github.com>
2019-06-13 11:23:37 -0400

    terro
    logion

--- conexion.js
+++ conexion.js
@@ -2,8 +2,123 @@
 firebase.initializeApp({
     apiKey: "AIzaSyDGcEqhdJ_U6csAQTQLlxg0x3Ks3TCEhzo",
     authDomain: "conexione-5bb09.firebaseapp.com",
-    projectId: "conexione-5bb09"
+    databaseURL: "https://conexione-5bb09.firebaseio.com",
+    projectId: "conexione-5bb09",
+    storageBucket: "conexione-5bb09.appspot.com",
+    messagingSenderId: "472627438430"
+
   });
+
+  //login
+  
+
+// Connect application with firebase
+const form = document.forms['loginForm'];
+firebase.auth().onAuthStateChanged(handleAuthState);
+form.addEventListener('submit', handleFormSubmit);
+
+
+// Application defs
+function handleAuthState(user) {
+  if (user) {
+    showPrivateInfo()
+    return console.log('Felisitaciones entraste 🎉');
+  }
+
+  showLoginForm()
+  return console.log('No tenemos ningún usuario 😭');
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const email = form['email'].value;
+  const password = form['password'].value;
+  const isLoginOrSignup = form['isLoginOrSignup'].value;
+
+  if (isLoginOrSignup === 'isLogin') {
+    return loginUser({ email, password });
+  }
+
+  return createUser({ email, password });
+}
+
+
+// Application Utils
+function showPrivateInfo(user) {
+  const loginForm = document.getElementById('loginFormUI');
+  loginForm.style.display = 'none';
+
+  const hiddenPrivateInfo = document.getElementById('hiddenPrivateInfo');
+  hiddenPrivateInfo.style.display = 'block';
+  hiddenPrivateInfo.innerHTML = `
+    <p>Esto <b>SI</b> es información confidencial </p>
+    <button id="btnLogout" class="button">Logout</button>
+  `;
+
+  const btnLogout = document.getElementById('btnLogout');
+  btnLogout.addEventListener('click', signoutUser);
+}
+
+function showLoginForm() {
+  const loginForm = document.getElementById('loginFormUI');
+  loginForm.style.display = 'block';
+
+  const hiddenPrivateInfo = document.getElementById('hiddenPrivateInfo');
+  hiddenPrivateInfo.style.display = 'none';
+  hiddenPrivateInfo.innerHTML = `
+    <p>Nada que mostrar, tenes que logearte, bro...</p>
+  `;
+}
+
+
+// Firebase defs
+function createUser({ email, password }) {
+  console.log('Creating user ' + email);
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function (user) {
+      console.log('¡Creamos el user, bro! Huepaje!');
+    })
+    .catch(function (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('Ya existe el usuario');
+        const soLogin = confirm(
+          `Ya te habias registrado con este email, bro 😝.
+          ¿Quieres iniciar sesión ✨?`
+        );
+        return !!soLogin ? loginUser({ email, password }) : alertTryAgain(error);;
+      }
+
+      return alertTryAgain(error);
+    });
+}
+
+function loginUser({ email, password }) {
+  console.log('Loging user ' + email);
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+      console.log('Credenciales correctas, brother, bienvenido.');
+    })
+    .catch(function (error) {
+      console.log(error);
+      alertTryAgain(error);
+    });
+}
+
+function signoutUser() {
+  firebase.auth().signOut();
+}
+
+
+// General Utils
+function alertTryAgain(error) {
+  console.log(error);
+  return alert('Error, intenta de nuevo ⛈');
+}
+
+//fin de login
   
   // Initialize Cloud Firestore through Firebase
   var db = firebase.firestore();
@@ -46,6 +161,3 @@ firebase.initializeApp({
         });
     });
 }
-
- 
-  
